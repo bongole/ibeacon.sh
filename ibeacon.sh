c041776875e96e64a72af07402ce3437aff2bebd
Georg Lukas <georg@op-co.de>
2014-07-01 10:12:48 +0200

    Activate and advertize the BLE device

--- ibeacon.sh
+++ ibeacon.sh
@@ -10,5 +10,6 @@ TMP_MAJOR=$(printf '%04X' $MAJOR|sed 's/\(..\)/\1 /g')
 TMP_MINOR=$(printf '%04X' $MINOR|sed 's/\(..\)/\1 /g')
 TMP_MEASURED_POWER=$(printf '%0X' $MEASURED_POWER)
 
+hciconfig $BT_DEV up
 hcitool -i $BT_DEV cmd 0x08 0x0008 1f 02 01 1a 1a ff 4c 00 02 15 $TMP_UUID$TMP_MAJOR$TMP_MINOR$TMP_MEASURED_POWER 00
-#hcitool -i hci1 cmd 0x08 0x0008 1f 02 01 1a 1a ff 4c 00 02 15 03 54 d9 b9 8a 76 47 8f 9d ee cc dd 7e b6 d0 fa 00 00 00 00 32 00
+hciconfig $BT_DEV leadv 3
